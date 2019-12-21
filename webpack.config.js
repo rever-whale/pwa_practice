28ee5f5af9e3b86de7f6a3dc04ffdfd5748b649f
cayde-abdo <cayde.abdo@kakaocorp.com>
2019-12-21 15:14:32 +0900

    2. initial setting

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, './src/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html')
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
